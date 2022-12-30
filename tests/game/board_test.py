e63621c636ec796290f4af834e588cdbd7f77eeb
Joona Kauppila <joona.kauppila@helsinki.fi>
2022-12-30 13:30:52 +0200

    multiple tile and combine move tests

--- tests/game/board_test.py
+++ tests/game/board_test.py
@@ -41,3 +41,63 @@ class TestBoardMoves:
         assert board.state == BoardState.INPROGRESS
         assert board.score == 4
         assert board.last_move == Direction.RIGHT
+
+    # Multiple tiles
+
+    def test_move_up_multiple_tiles(self):
+        board = Board(123, initial=np.array([[0,0,0,0],[0,0,4,2],[0,0,0,8],[0,128,0,0]]))
+        board.move(Direction.UP)
+        desired = np.array([[2,128,4,2],[0,0,0,8],[0,0,0,0],[0,0,0,0]])
+        assert np.array_equal(desired, board.board)
+        assert board.score == 0
+
+    def test_move_down_multiple_tiles(self):
+        board = Board(123, initial=np.array([[0,0,0,0],[0,0,4,2],[0,0,0,8],[0,128,0,0]]))
+        board.move(Direction.DOWN)
+        desired = np.array([[2,0,0,0],[0,0,0,0],[0,0,0,2],[0,128,4,8]])
+        assert np.array_equal(desired, board.board)
+        assert board.score == 0
+
+    def test_move_left_multiple_tiles(self):
+        board = Board(123, initial=np.array([[0,0,0,0],[0,0,4,2],[0,0,0,8],[0,128,0,0]]))
+        board.move(Direction.LEFT)
+        desired = np.array([[2,0,0,0],[4,2,0,0],[8,0,0,0],[128,0,0,0]])
+        assert np.array_equal(desired, board.board)
+        assert board.score == 0
+
+    def test_move_right_multiple_tiles(self):
+        board = Board(123, initial=np.array([[0,0,0,0],[0,0,4,2],[0,0,0,8],[0,128,0,0]]))
+        board.move(Direction.RIGHT)
+        desired = np.array([[2,0,0,0],[0,0,4,2],[0,0,0,8],[0,0,0,128]])
+        assert np.array_equal(desired, board.board)
+        assert board.score == 0
+
+    # Combining tiles
+
+    def test_move_up_combine_tiles(self):
+        board = Board(123, initial=np.array([[0,0,0,0],[0,0,64,0],[0,0,64,0],[0,0,0,0]]))
+        board.move(Direction.UP)
+        desired = np.array([[2,0,128,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
+        assert np.array_equal(desired, board.board)
+        assert board.score == 128
+
+    def test_move_down_combine_tiles(self):
+        board = Board(123, initial=np.array([[0,0,0,0],[0,16,0,0],[0,16,0,0],[0,0,0,0]]))
+        board.move(Direction.DOWN)
+        desired = np.array([[2,0,0,0],[0,0,0,0],[0,0,0,0],[0,32,0,0]])
+        assert np.array_equal(desired, board.board)
+        assert board.score == 32
+
+    def test_move_left_combine_tiles(self):
+        board = Board(123, initial=np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,2,2]]))
+        board.move(Direction.LEFT)
+        desired = np.array([[2,0,0,0],[0,0,0,0],[0,0,0,0],[4,0,0,0]])
+        assert np.array_equal(desired, board.board)
+        assert board.score == 4
+
+    def test_move_right_combine_tiles(self):
+        board = Board(123, initial=np.array([[0,0,0,0],[0,512,512,0],[0,0,0,0],[0,0,0,0]]))
+        board.move(Direction.RIGHT)
+        desired = np.array([[2,0,0,0],[0,0,0,1024],[0,0,0,0],[0,0,0,0]])
+        assert np.array_equal(desired, board.board)
+        assert board.score == 1024
