51ded0eeede9f87a66982d87dd4f74ebf46ff2e0
Joona Kauppila <joona.kauppila@helsinki.fi>
2022-12-31 10:55:03 +0200

    random ai tests

---
+++ tests/ai/random_test.py
@@ -0,0 +1,22 @@
+import pytest
+import numpy as np
+from game.board import Board
+from ai.random_ai import RandomAI
+from game.board_utils import Direction, BoardState
+
+
+class TestRandomMoves:
+
+    @pytest.fixture(autouse=True)
+    def algorithm(self):
+        self.algorithm = RandomAI()
+
+    def test_random(self):
+        board = Board(123)
+        moves = []
+        for _ in range(200):
+            move = self.algorithm.get_move(board)
+            assert move in [Direction.RIGHT, Direction.LEFT, Direction.UP, Direction.DOWN]
+            moves.append(move)
+        assert Direction.UP in moves and Direction.RIGHT in moves
+
